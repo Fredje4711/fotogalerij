e3f2083a607c8b76f1fa10ce294f2048809ec62a
Fredje4711 <fredje4711@gmail.com>
2025-05-09 20:12:30 +0200

    Add files via upload

---
+++ Infosessie CardioVasculair 13-03-2025/gallery-data.js
@@ -0,0 +1,74 @@
+const photoGalleryData = [
+    {
+        id: "foto1",
+        full: "images/full/1_full.jpg",
+        thumb: "images/thumbnails/1_thumb.jpg",
+        title: "Infosessie CardioVasculair 13-03-2025 - Foto 1",
+        description: "",
+        alt: "Infosessie CardioVasculair 13-03-2025 - Foto 1"
+    },
+    {
+        id: "foto2",
+        full: "images/full/2_full.jpg",
+        thumb: "images/thumbnails/2_thumb.jpg",
+        title: "Infosessie CardioVasculair 13-03-2025 - Foto 2",
+        description: "",
+        alt: "Infosessie CardioVasculair 13-03-2025 - Foto 2"
+    },
+    {
+        id: "foto3",
+        full: "images/full/3_full.jpg",
+        thumb: "images/thumbnails/3_thumb.jpg",
+        title: "Infosessie CardioVasculair 13-03-2025 - Foto 3",
+        description: "",
+        alt: "Infosessie CardioVasculair 13-03-2025 - Foto 3"
+    },
+    {
+        id: "foto4",
+        full: "images/full/4_full.jpg",
+        thumb: "images/thumbnails/4_thumb.jpg",
+        title: "Infosessie CardioVasculair 13-03-2025 - Foto 4",
+        description: "",
+        alt: "Infosessie CardioVasculair 13-03-2025 - Foto 4"
+    },
+    {
+        id: "foto5",
+        full: "images/full/5_full.jpg",
+        thumb: "images/thumbnails/5_thumb.jpg",
+        title: "Infosessie CardioVasculair 13-03-2025 - Foto 5",
+        description: "",
+        alt: "Infosessie CardioVasculair 13-03-2025 - Foto 5"
+    },
+    {
+        id: "foto6",
+        full: "images/full/6_full.jpg",
+        thumb: "images/thumbnails/6_thumb.jpg",
+        title: "Infosessie CardioVasculair 13-03-2025 - Foto 6",
+        description: "",
+        alt: "Infosessie CardioVasculair 13-03-2025 - Foto 6"
+    },
+    {
+        id: "foto7",
+        full: "images/full/7_full.jpg",
+        thumb: "images/thumbnails/7_thumb.jpg",
+        title: "Infosessie CardioVasculair 13-03-2025 - Foto 7",
+        description: "",
+        alt: "Infosessie CardioVasculair 13-03-2025 - Foto 7"
+    },
+    {
+        id: "foto8",
+        full: "images/full/8_full.jpg",
+        thumb: "images/thumbnails/8_thumb.jpg",
+        title: "Infosessie CardioVasculair 13-03-2025 - Foto 8",
+        description: "",
+        alt: "Infosessie CardioVasculair 13-03-2025 - Foto 8"
+    },
+    {
+        id: "foto9",
+        full: "images/full/9_full.jpg",
+        thumb: "images/thumbnails/9_thumb.jpg",
+        title: "Infosessie CardioVasculair 13-03-2025 - Foto 9",
+        description: "",
+        alt: "Infosessie CardioVasculair 13-03-2025 - Foto 9"
+    }
+];
