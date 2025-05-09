ea30e5988121578dfb6401da1a4ac277be546b8e
Fredje4711 <fredje4711@gmail.com>
2025-05-09 21:51:46 +0200

    Add files via upload

--- Infosessie CardioVasculair 13-03-2025/gallery-data.js
+++ Infosessie CardioVasculair 13-03-2025/gallery-data.js
@@ -3,72 +3,72 @@ const photoGalleryData = [
         id: "foto1",
         full: "images/full/1_full.jpg",
         thumb: "images/thumbnails/1_thumb.jpg",
-        title: "Infosessie CardioVasculair 13-03-2025 - Foto 1",
+        title: "20250313_201305 (Foto 1)",
         description: "",
-        alt: "Infosessie CardioVasculair 13-03-2025 - Foto 1"
+        alt: "20250313_201305 (Foto 1)"
     },
     {
         id: "foto2",
         full: "images/full/2_full.jpg",
         thumb: "images/thumbnails/2_thumb.jpg",
-        title: "Infosessie CardioVasculair 13-03-2025 - Foto 2",
+        title: "20250313_201330 (Foto 2)",
         description: "",
-        alt: "Infosessie CardioVasculair 13-03-2025 - Foto 2"
+        alt: "20250313_201330 (Foto 2)"
     },
     {
         id: "foto3",
         full: "images/full/3_full.jpg",
         thumb: "images/thumbnails/3_thumb.jpg",
-        title: "Infosessie CardioVasculair 13-03-2025 - Foto 3",
+        title: "20250313_202539 (Foto 3)",
         description: "",
-        alt: "Infosessie CardioVasculair 13-03-2025 - Foto 3"
+        alt: "20250313_202539 (Foto 3)"
     },
     {
         id: "foto4",
         full: "images/full/4_full.jpg",
         thumb: "images/thumbnails/4_thumb.jpg",
-        title: "Infosessie CardioVasculair 13-03-2025 - Foto 4",
+        title: "20250313_203652 (Foto 4)",
         description: "",
-        alt: "Infosessie CardioVasculair 13-03-2025 - Foto 4"
+        alt: "20250313_203652 (Foto 4)"
     },
     {
         id: "foto5",
         full: "images/full/5_full.jpg",
         thumb: "images/thumbnails/5_thumb.jpg",
-        title: "Infosessie CardioVasculair 13-03-2025 - Foto 5",
+        title: "20250313_204435 (Foto 5)",
         description: "",
-        alt: "Infosessie CardioVasculair 13-03-2025 - Foto 5"
+        alt: "20250313_204435 (Foto 5)"
     },
     {
         id: "foto6",
         full: "images/full/6_full.jpg",
         thumb: "images/thumbnails/6_thumb.jpg",
-        title: "Infosessie CardioVasculair 13-03-2025 - Foto 6",
+        title: "20250313_204439 (Foto 6)",
         description: "",
-        alt: "Infosessie CardioVasculair 13-03-2025 - Foto 6"
+        alt: "20250313_204439 (Foto 6)"
     },
     {
         id: "foto7",
         full: "images/full/7_full.jpg",
         thumb: "images/thumbnails/7_thumb.jpg",
-        title: "Infosessie CardioVasculair 13-03-2025 - Foto 7",
+        title: "20250313_204445 (Foto 7)",
         description: "",
-        alt: "Infosessie CardioVasculair 13-03-2025 - Foto 7"
+        alt: "20250313_204445 (Foto 7)"
     },
     {
         id: "foto8",
         full: "images/full/8_full.jpg",
         thumb: "images/thumbnails/8_thumb.jpg",
-        title: "Infosessie CardioVasculair 13-03-2025 - Foto 8",
+        title: "De zotte Andre (Foto 8)",
         description: "",
-        alt: "Infosessie CardioVasculair 13-03-2025 - Foto 8"
+        alt: "De zotte Andre (Foto 8)"
     },
     {
         id: "foto9",
         full: "images/full/9_full.jpg",
         thumb: "images/thumbnails/9_thumb.jpg",
-        title: "Infosessie CardioVasculair 13-03-2025 - Foto 9",
+        title: "Toespraak Eddy (Foto 9)",
         description: "",
-        alt: "Infosessie CardioVasculair 13-03-2025 - Foto 9"
+        alt: "Toespraak Eddy (Foto 9)"
     }
 ];
