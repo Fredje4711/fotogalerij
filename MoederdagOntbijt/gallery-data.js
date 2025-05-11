c41c81f25888520f00a0e713ca1c8e351d190981
Fredje <fredje4711@gmail.com>
2025-05-11 14:14:17 +0200

    Voeg fotogalerij MoederdagOntbijt toe

---
+++ MoederdagOntbijt/gallery-data.js
@@ -0,0 +1,146 @@
+const photoGalleryData = [
+    {
+        id: "foto1",
+        full: "images/full/1_full.jpg",
+        thumb: "images/thumbnails/1_thumb.jpg",
+        title: "Moederdag Ontbijt - Foto 1",
+        description: "",
+        alt: "Moederdag Ontbijt - Foto 1"
+    },
+    {
+        id: "foto2",
+        full: "images/full/2_full.jpg",
+        thumb: "images/thumbnails/2_thumb.jpg",
+        title: "Moederdag Ontbijt - Foto 2",
+        description: "",
+        alt: "Moederdag Ontbijt - Foto 2"
+    },
+    {
+        id: "foto3",
+        full: "images/full/3_full.jpg",
+        thumb: "images/thumbnails/3_thumb.jpg",
+        title: "Moederdag Ontbijt - Foto 3",
+        description: "",
+        alt: "Moederdag Ontbijt - Foto 3"
+    },
+    {
+        id: "foto4",
+        full: "images/full/4_full.jpg",
+        thumb: "images/thumbnails/4_thumb.jpg",
+        title: "Moederdag Ontbijt - Foto 4",
+        description: "",
+        alt: "Moederdag Ontbijt - Foto 4"
+    },
+    {
+        id: "foto5",
+        full: "images/full/5_full.jpg",
+        thumb: "images/thumbnails/5_thumb.jpg",
+        title: "Moederdag Ontbijt - Foto 5",
+        description: "",
+        alt: "Moederdag Ontbijt - Foto 5"
+    },
+    {
+        id: "foto6",
+        full: "images/full/6_full.jpg",
+        thumb: "images/thumbnails/6_thumb.jpg",
+        title: "Moederdag Ontbijt - Foto 6",
+        description: "",
+        alt: "Moederdag Ontbijt - Foto 6"
+    },
+    {
+        id: "foto7",
+        full: "images/full/7_full.jpg",
+        thumb: "images/thumbnails/7_thumb.jpg",
+        title: "Moederdag Ontbijt - Foto 7",
+        description: "",
+        alt: "Moederdag Ontbijt - Foto 7"
+    },
+    {
+        id: "foto8",
+        full: "images/full/8_full.jpg",
+        thumb: "images/thumbnails/8_thumb.jpg",
+        title: "Moederdag Ontbijt - Foto 8",
+        description: "",
+        alt: "Moederdag Ontbijt - Foto 8"
+    },
+    {
+        id: "foto9",
+        full: "images/full/9_full.jpg",
+        thumb: "images/thumbnails/9_thumb.jpg",
+        title: "Moederdag Ontbijt - Foto 9",
+        description: "",
+        alt: "Moederdag Ontbijt - Foto 9"
+    },
+    {
+        id: "foto10",
+        full: "images/full/10_full.jpg",
+        thumb: "images/thumbnails/10_thumb.jpg",
+        title: "Moederdag Ontbijt - Foto 10",
+        description: "",
+        alt: "Moederdag Ontbijt - Foto 10"
+    },
+    {
+        id: "foto11",
+        full: "images/full/11_full.jpg",
+        thumb: "images/thumbnails/11_thumb.jpg",
+        title: "Moederdag Ontbijt - Foto 11",
+        description: "",
+        alt: "Moederdag Ontbijt - Foto 11"
+    },
+    {
+        id: "foto12",
+        full: "images/full/12_full.jpg",
+        thumb: "images/thumbnails/12_thumb.jpg",
+        title: "Moederdag Ontbijt - Foto 12",
+        description: "",
+        alt: "Moederdag Ontbijt - Foto 12"
+    },
+    {
+        id: "foto13",
+        full: "images/full/13_full.jpg",
+        thumb: "images/thumbnails/13_thumb.jpg",
+        title: "Moederdag Ontbijt - Foto 13",
+        description: "",
+        alt: "Moederdag Ontbijt - Foto 13"
+    },
+    {
+        id: "foto14",
+        full: "images/full/14_full.jpg",
+        thumb: "images/thumbnails/14_thumb.jpg",
+        title: "Moederdag Ontbijt - Foto 14",
+        description: "",
+        alt: "Moederdag Ontbijt - Foto 14"
+    },
+    {
+        id: "foto15",
+        full: "images/full/15_full.jpg",
+        thumb: "images/thumbnails/15_thumb.jpg",
+        title: "Moederdag Ontbijt - Foto 15",
+        description: "",
+        alt: "Moederdag Ontbijt - Foto 15"
+    },
+    {
+        id: "foto16",
+        full: "images/full/16_full.jpg",
+        thumb: "images/thumbnails/16_thumb.jpg",
+        title: "Moederdag Ontbijt - Foto 16",
+        description: "",
+        alt: "Moederdag Ontbijt - Foto 16"
+    },
+    {
+        id: "foto17",
+        full: "images/full/17_full.jpg",
+        thumb: "images/thumbnails/17_thumb.jpg",
+        title: "Moederdag Ontbijt - Foto 17",
+        description: "",
+        alt: "Moederdag Ontbijt - Foto 17"
+    },
+    {
+        id: "foto18",
+        full: "images/full/18_full.jpg",
+        thumb: "images/thumbnails/18_thumb.jpg",
+        title: "Moederdag Ontbijt - Foto 18",
+        description: "",
+        alt: "Moederdag Ontbijt - Foto 18"
+    }
+];
