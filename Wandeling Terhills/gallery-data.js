ef9eb5b2dd930271b737d51bea6cc67c044a73bb
Fredje4711 <fredje4711@gmail.com>
2025-05-20 19:05:10 +0200

    Add files via upload

---
+++ Wandeling Terhills/gallery-data.js
@@ -0,0 +1,602 @@
+const photoGalleryData = [
+    {
+        id: "foto1",
+        full: "images/full/1_full.jpg",
+        thumb: "images/thumbnails/1_thumb.jpg",
+        title: "Wandeling Terhills - Foto 1",
+        description: "",
+        alt: "Wandeling Terhills - Foto 1"
+    },
+    {
+        id: "foto2",
+        full: "images/full/2_full.jpg",
+        thumb: "images/thumbnails/2_thumb.jpg",
+        title: "Wandeling Terhills - Foto 2",
+        description: "",
+        alt: "Wandeling Terhills - Foto 2"
+    },
+    {
+        id: "foto3",
+        full: "images/full/3_full.jpg",
+        thumb: "images/thumbnails/3_thumb.jpg",
+        title: "Wandeling Terhills - Foto 3",
+        description: "",
+        alt: "Wandeling Terhills - Foto 3"
+    },
+    {
+        id: "foto4",
+        full: "images/full/4_full.jpg",
+        thumb: "images/thumbnails/4_thumb.jpg",
+        title: "Wandeling Terhills - Foto 4",
+        description: "",
+        alt: "Wandeling Terhills - Foto 4"
+    },
+    {
+        id: "foto5",
+        full: "images/full/5_full.jpg",
+        thumb: "images/thumbnails/5_thumb.jpg",
+        title: "Wandeling Terhills - Foto 5",
+        description: "",
+        alt: "Wandeling Terhills - Foto 5"
+    },
+    {
+        id: "foto6",
+        full: "images/full/6_full.jpg",
+        thumb: "images/thumbnails/6_thumb.jpg",
+        title: "Wandeling Terhills - Foto 6",
+        description: "",
+        alt: "Wandeling Terhills - Foto 6"
+    },
+    {
+        id: "foto7",
+        full: "images/full/7_full.jpg",
+        thumb: "images/thumbnails/7_thumb.jpg",
+        title: "Wandeling Terhills - Foto 7",
+        description: "",
+        alt: "Wandeling Terhills - Foto 7"
+    },
+    {
+        id: "foto8",
+        full: "images/full/8_full.jpg",
+        thumb: "images/thumbnails/8_thumb.jpg",
+        title: "Wandeling Terhills - Foto 8",
+        description: "",
+        alt: "Wandeling Terhills - Foto 8"
+    },
+    {
+        id: "foto9",
+        full: "images/full/9_full.jpg",
+        thumb: "images/thumbnails/9_thumb.jpg",
+        title: "Wandeling Terhills - Foto 9",
+        description: "",
+        alt: "Wandeling Terhills - Foto 9"
+    },
+    {
+        id: "foto10",
+        full: "images/full/10_full.jpg",
+        thumb: "images/thumbnails/10_thumb.jpg",
+        title: "Wandeling Terhills - Foto 10",
+        description: "",
+        alt: "Wandeling Terhills - Foto 10"
+    },
+    {
+        id: "foto11",
+        full: "images/full/11_full.jpg",
+        thumb: "images/thumbnails/11_thumb.jpg",
+        title: "Wandeling Terhills - Foto 11",
+        description: "",
+        alt: "Wandeling Terhills - Foto 11"
+    },
+    {
+        id: "foto12",
+        full: "images/full/12_full.jpg",
+        thumb: "images/thumbnails/12_thumb.jpg",
+        title: "Wandeling Terhills - Foto 12",
+        description: "",
+        alt: "Wandeling Terhills - Foto 12"
+    },
+    {
+        id: "foto13",
+        full: "images/full/13_full.jpg",
+        thumb: "images/thumbnails/13_thumb.jpg",
+        title: "Wandeling Terhills - Foto 13",
+        description: "",
+        alt: "Wandeling Terhills - Foto 13"
+    },
+    {
+        id: "foto14",
+        full: "images/full/14_full.jpg",
+        thumb: "images/thumbnails/14_thumb.jpg",
+        title: "Wandeling Terhills - Foto 14",
+        description: "",
+        alt: "Wandeling Terhills - Foto 14"
+    },
+    {
+        id: "foto15",
+        full: "images/full/15_full.jpg",
+        thumb: "images/thumbnails/15_thumb.jpg",
+        title: "Wandeling Terhills - Foto 15",
+        description: "",
+        alt: "Wandeling Terhills - Foto 15"
+    },
+    {
+        id: "foto16",
+        full: "images/full/16_full.jpg",
+        thumb: "images/thumbnails/16_thumb.jpg",
+        title: "Wandeling Terhills - Foto 16",
+        description: "",
+        alt: "Wandeling Terhills - Foto 16"
+    },
+    {
+        id: "foto17",
+        full: "images/full/17_full.jpg",
+        thumb: "images/thumbnails/17_thumb.jpg",
+        title: "Wandeling Terhills - Foto 17",
+        description: "",
+        alt: "Wandeling Terhills - Foto 17"
+    },
+    {
+        id: "foto18",
+        full: "images/full/18_full.jpg",
+        thumb: "images/thumbnails/18_thumb.jpg",
+        title: "Wandeling Terhills - Foto 18",
+        description: "",
+        alt: "Wandeling Terhills - Foto 18"
+    },
+    {
+        id: "foto19",
+        full: "images/full/19_full.jpg",
+        thumb: "images/thumbnails/19_thumb.jpg",
+        title: "Wandeling Terhills - Foto 19",
+        description: "",
+        alt: "Wandeling Terhills - Foto 19"
+    },
+    {
+        id: "foto20",
+        full: "images/full/20_full.jpg",
+        thumb: "images/thumbnails/20_thumb.jpg",
+        title: "Wandeling Terhills - Foto 20",
+        description: "",
+        alt: "Wandeling Terhills - Foto 20"
+    },
+    {
+        id: "foto21",
+        full: "images/full/21_full.jpg",
+        thumb: "images/thumbnails/21_thumb.jpg",
+        title: "Wandeling Terhills - Foto 21",
+        description: "",
+        alt: "Wandeling Terhills - Foto 21"
+    },
+    {
+        id: "foto22",
+        full: "images/full/22_full.jpg",
+        thumb: "images/thumbnails/22_thumb.jpg",
+        title: "Wandeling Terhills - Foto 22",
+        description: "",
+        alt: "Wandeling Terhills - Foto 22"
+    },
+    {
+        id: "foto23",
+        full: "images/full/23_full.jpg",
+        thumb: "images/thumbnails/23_thumb.jpg",
+        title: "Wandeling Terhills - Foto 23",
+        description: "",
+        alt: "Wandeling Terhills - Foto 23"
+    },
+    {
+        id: "foto24",
+        full: "images/full/24_full.jpg",
+        thumb: "images/thumbnails/24_thumb.jpg",
+        title: "Wandeling Terhills - Foto 24",
+        description: "",
+        alt: "Wandeling Terhills - Foto 24"
+    },
+    {
+        id: "foto25",
+        full: "images/full/25_full.jpg",
+        thumb: "images/thumbnails/25_thumb.jpg",
+        title: "Wandeling Terhills - Foto 25",
+        description: "",
+        alt: "Wandeling Terhills - Foto 25"
+    },
+    {
+        id: "foto26",
+        full: "images/full/26_full.jpg",
+        thumb: "images/thumbnails/26_thumb.jpg",
+        title: "Wandeling Terhills - Foto 26",
+        description: "",
+        alt: "Wandeling Terhills - Foto 26"
+    },
+    {
+        id: "foto27",
+        full: "images/full/27_full.jpg",
+        thumb: "images/thumbnails/27_thumb.jpg",
+        title: "Wandeling Terhills - Foto 27",
+        description: "",
+        alt: "Wandeling Terhills - Foto 27"
+    },
+    {
+        id: "foto28",
+        full: "images/full/28_full.jpg",
+        thumb: "images/thumbnails/28_thumb.jpg",
+        title: "Wandeling Terhills - Foto 28",
+        description: "",
+        alt: "Wandeling Terhills - Foto 28"
+    },
+    {
+        id: "foto29",
+        full: "images/full/29_full.jpg",
+        thumb: "images/thumbnails/29_thumb.jpg",
+        title: "Wandeling Terhills - Foto 29",
+        description: "",
+        alt: "Wandeling Terhills - Foto 29"
+    },
+    {
+        id: "foto30",
+        full: "images/full/30_full.jpg",
+        thumb: "images/thumbnails/30_thumb.jpg",
+        title: "Wandeling Terhills - Foto 30",
+        description: "",
+        alt: "Wandeling Terhills - Foto 30"
+    },
+    {
+        id: "foto31",
+        full: "images/full/31_full.jpg",
+        thumb: "images/thumbnails/31_thumb.jpg",
+        title: "Wandeling Terhills - Foto 31",
+        description: "",
+        alt: "Wandeling Terhills - Foto 31"
+    },
+    {
+        id: "foto32",
+        full: "images/full/32_full.jpg",
+        thumb: "images/thumbnails/32_thumb.jpg",
+        title: "Wandeling Terhills - Foto 32",
+        description: "",
+        alt: "Wandeling Terhills - Foto 32"
+    },
+    {
+        id: "foto33",
+        full: "images/full/33_full.jpg",
+        thumb: "images/thumbnails/33_thumb.jpg",
+        title: "Wandeling Terhills - Foto 33",
+        description: "",
+        alt: "Wandeling Terhills - Foto 33"
+    },
+    {
+        id: "foto34",
+        full: "images/full/34_full.jpg",
+        thumb: "images/thumbnails/34_thumb.jpg",
+        title: "Wandeling Terhills - Foto 34",
+        description: "",
+        alt: "Wandeling Terhills - Foto 34"
+    },
+    {
+        id: "foto35",
+        full: "images/full/35_full.jpg",
+        thumb: "images/thumbnails/35_thumb.jpg",
+        title: "Wandeling Terhills - Foto 35",
+        description: "",
+        alt: "Wandeling Terhills - Foto 35"
+    },
+    {
+        id: "foto36",
+        full: "images/full/36_full.jpg",
+        thumb: "images/thumbnails/36_thumb.jpg",
+        title: "Wandeling Terhills - Foto 36",
+        description: "",
+        alt: "Wandeling Terhills - Foto 36"
+    },
+    {
+        id: "foto37",
+        full: "images/full/37_full.jpg",
+        thumb: "images/thumbnails/37_thumb.jpg",
+        title: "Wandeling Terhills - Foto 37",
+        description: "",
+        alt: "Wandeling Terhills - Foto 37"
+    },
+    {
+        id: "foto38",
+        full: "images/full/38_full.jpg",
+        thumb: "images/thumbnails/38_thumb.jpg",
+        title: "Wandeling Terhills - Foto 38",
+        description: "",
+        alt: "Wandeling Terhills - Foto 38"
+    },
+    {
+        id: "foto39",
+        full: "images/full/39_full.jpg",
+        thumb: "images/thumbnails/39_thumb.jpg",
+        title: "Wandeling Terhills - Foto 39",
+        description: "",
+        alt: "Wandeling Terhills - Foto 39"
+    },
+    {
+        id: "foto40",
+        full: "images/full/40_full.jpg",
+        thumb: "images/thumbnails/40_thumb.jpg",
+        title: "Wandeling Terhills - Foto 40",
+        description: "",
+        alt: "Wandeling Terhills - Foto 40"
+    },
+    {
+        id: "foto41",
+        full: "images/full/41_full.jpg",
+        thumb: "images/thumbnails/41_thumb.jpg",
+        title: "Wandeling Terhills - Foto 41",
+        description: "",
+        alt: "Wandeling Terhills - Foto 41"
+    },
+    {
+        id: "foto42",
+        full: "images/full/42_full.jpg",
+        thumb: "images/thumbnails/42_thumb.jpg",
+        title: "Wandeling Terhills - Foto 42",
+        description: "",
+        alt: "Wandeling Terhills - Foto 42"
+    },
+    {
+        id: "foto43",
+        full: "images/full/43_full.jpg",
+        thumb: "images/thumbnails/43_thumb.jpg",
+        title: "Wandeling Terhills - Foto 43",
+        description: "",
+        alt: "Wandeling Terhills - Foto 43"
+    },
+    {
+        id: "foto44",
+        full: "images/full/44_full.jpg",
+        thumb: "images/thumbnails/44_thumb.jpg",
+        title: "Wandeling Terhills - Foto 44",
+        description: "",
+        alt: "Wandeling Terhills - Foto 44"
+    },
+    {
+        id: "foto45",
+        full: "images/full/45_full.jpg",
+        thumb: "images/thumbnails/45_thumb.jpg",
+        title: "Wandeling Terhills - Foto 45",
+        description: "",
+        alt: "Wandeling Terhills - Foto 45"
+    },
+    {
+        id: "foto46",
+        full: "images/full/46_full.jpg",
+        thumb: "images/thumbnails/46_thumb.jpg",
+        title: "Wandeling Terhills - Foto 46",
+        description: "",
+        alt: "Wandeling Terhills - Foto 46"
+    },
+    {
+        id: "foto47",
+        full: "images/full/47_full.jpg",
+        thumb: "images/thumbnails/47_thumb.jpg",
+        title: "Wandeling Terhills - Foto 47",
+        description: "",
+        alt: "Wandeling Terhills - Foto 47"
+    },
+    {
+        id: "foto48",
+        full: "images/full/48_full.jpg",
+        thumb: "images/thumbnails/48_thumb.jpg",
+        title: "Wandeling Terhills - Foto 48",
+        description: "",
+        alt: "Wandeling Terhills - Foto 48"
+    },
+    {
+        id: "foto49",
+        full: "images/full/49_full.jpg",
+        thumb: "images/thumbnails/49_thumb.jpg",
+        title: "Wandeling Terhills - Foto 49",
+        description: "",
+        alt: "Wandeling Terhills - Foto 49"
+    },
+    {
+        id: "foto50",
+        full: "images/full/50_full.jpg",
+        thumb: "images/thumbnails/50_thumb.jpg",
+        title: "Wandeling Terhills - Foto 50",
+        description: "",
+        alt: "Wandeling Terhills - Foto 50"
+    },
+    {
+        id: "foto51",
+        full: "images/full/51_full.jpg",
+        thumb: "images/thumbnails/51_thumb.jpg",
+        title: "Wandeling Terhills - Foto 51",
+        description: "",
+        alt: "Wandeling Terhills - Foto 51"
+    },
+    {
+        id: "foto52",
+        full: "images/full/52_full.jpg",
+        thumb: "images/thumbnails/52_thumb.jpg",
+        title: "Wandeling Terhills - Foto 52",
+        description: "",
+        alt: "Wandeling Terhills - Foto 52"
+    },
+    {
+        id: "foto53",
+        full: "images/full/53_full.jpg",
+        thumb: "images/thumbnails/53_thumb.jpg",
+        title: "Wandeling Terhills - Foto 53",
+        description: "",
+        alt: "Wandeling Terhills - Foto 53"
+    },
+    {
+        id: "foto54",
+        full: "images/full/54_full.jpg",
+        thumb: "images/thumbnails/54_thumb.jpg",
+        title: "Wandeling Terhills - Foto 54",
+        description: "",
+        alt: "Wandeling Terhills - Foto 54"
+    },
+    {
+        id: "foto55",
+        full: "images/full/55_full.jpg",
+        thumb: "images/thumbnails/55_thumb.jpg",
+        title: "Wandeling Terhills - Foto 55",
+        description: "",
+        alt: "Wandeling Terhills - Foto 55"
+    },
+    {
+        id: "foto56",
+        full: "images/full/56_full.jpg",
+        thumb: "images/thumbnails/56_thumb.jpg",
+        title: "Wandeling Terhills - Foto 56",
+        description: "",
+        alt: "Wandeling Terhills - Foto 56"
+    },
+    {
+        id: "foto57",
+        full: "images/full/57_full.jpg",
+        thumb: "images/thumbnails/57_thumb.jpg",
+        title: "Wandeling Terhills - Foto 57",
+        description: "",
+        alt: "Wandeling Terhills - Foto 57"
+    },
+    {
+        id: "foto58",
+        full: "images/full/58_full.jpg",
+        thumb: "images/thumbnails/58_thumb.jpg",
+        title: "Wandeling Terhills - Foto 58",
+        description: "",
+        alt: "Wandeling Terhills - Foto 58"
+    },
+    {
+        id: "foto59",
+        full: "images/full/59_full.jpg",
+        thumb: "images/thumbnails/59_thumb.jpg",
+        title: "Wandeling Terhills - Foto 59",
+        description: "",
+        alt: "Wandeling Terhills - Foto 59"
+    },
+    {
+        id: "foto60",
+        full: "images/full/60_full.jpg",
+        thumb: "images/thumbnails/60_thumb.jpg",
+        title: "Wandeling Terhills - Foto 60",
+        description: "",
+        alt: "Wandeling Terhills - Foto 60"
+    },
+    {
+        id: "foto61",
+        full: "images/full/61_full.jpg",
+        thumb: "images/thumbnails/61_thumb.jpg",
+        title: "Wandeling Terhills - Foto 61",
+        description: "",
+        alt: "Wandeling Terhills - Foto 61"
+    },
+    {
+        id: "foto62",
+        full: "images/full/62_full.jpg",
+        thumb: "images/thumbnails/62_thumb.jpg",
+        title: "Wandeling Terhills - Foto 62",
+        description: "",
+        alt: "Wandeling Terhills - Foto 62"
+    },
+    {
+        id: "foto63",
+        full: "images/full/63_full.jpg",
+        thumb: "images/thumbnails/63_thumb.jpg",
+        title: "Wandeling Terhills - Foto 63",
+        description: "",
+        alt: "Wandeling Terhills - Foto 63"
+    },
+    {
+        id: "foto64",
+        full: "images/full/64_full.jpg",
+        thumb: "images/thumbnails/64_thumb.jpg",
+        title: "Wandeling Terhills - Foto 64",
+        description: "",
+        alt: "Wandeling Terhills - Foto 64"
+    },
+    {
+        id: "foto65",
+        full: "images/full/65_full.jpg",
+        thumb: "images/thumbnails/65_thumb.jpg",
+        title: "Wandeling Terhills - Foto 65",
+        description: "",
+        alt: "Wandeling Terhills - Foto 65"
+    },
+    {
+        id: "foto66",
+        full: "images/full/66_full.jpg",
+        thumb: "images/thumbnails/66_thumb.jpg",
+        title: "Wandeling Terhills - Foto 66",
+        description: "",
+        alt: "Wandeling Terhills - Foto 66"
+    },
+    {
+        id: "foto67",
+        full: "images/full/67_full.jpg",
+        thumb: "images/thumbnails/67_thumb.jpg",
+        title: "Wandeling Terhills - Foto 67",
+        description: "",
+        alt: "Wandeling Terhills - Foto 67"
+    },
+    {
+        id: "foto68",
+        full: "images/full/68_full.jpg",
+        thumb: "images/thumbnails/68_thumb.jpg",
+        title: "Wandeling Terhills - Foto 68",
+        description: "",
+        alt: "Wandeling Terhills - Foto 68"
+    },
+    {
+        id: "foto69",
+        full: "images/full/69_full.jpg",
+        thumb: "images/thumbnails/69_thumb.jpg",
+        title: "Wandeling Terhills - Foto 69",
+        description: "",
+        alt: "Wandeling Terhills - Foto 69"
+    },
+    {
+        id: "foto70",
+        full: "images/full/70_full.jpg",
+        thumb: "images/thumbnails/70_thumb.jpg",
+        title: "Wandeling Terhills - Foto 70",
+        description: "",
+        alt: "Wandeling Terhills - Foto 70"
+    },
+    {
+        id: "foto71",
+        full: "images/full/71_full.jpg",
+        thumb: "images/thumbnails/71_thumb.jpg",
+        title: "Wandeling Terhills - Foto 71",
+        description: "",
+        alt: "Wandeling Terhills - Foto 71"
+    },
+    {
+        id: "foto72",
+        full: "images/full/72_full.jpg",
+        thumb: "images/thumbnails/72_thumb.jpg",
+        title: "Wandeling Terhills - Foto 72",
+        description: "",
+        alt: "Wandeling Terhills - Foto 72"
+    },
+    {
+        id: "foto73",
+        full: "images/full/73_full.jpg",
+        thumb: "images/thumbnails/73_thumb.jpg",
+        title: "Wandeling Terhills - Foto 73",
+        description: "",
+        alt: "Wandeling Terhills - Foto 73"
+    },
+    {
+        id: "foto74",
+        full: "images/full/74_full.jpg",
+        thumb: "images/thumbnails/74_thumb.jpg",
+        title: "Wandeling Terhills - Foto 74",
+        description: "",
+        alt: "Wandeling Terhills - Foto 74"
+    },
+    {
+        id: "foto75",
+        full: "images/full/75_full.jpg",
+        thumb: "images/thumbnails/75_thumb.jpg",
+        title: "Wandeling Terhills - Foto 75",
+        description: "",
+        alt: "Wandeling Terhills - Foto 75"
+    }
+];
